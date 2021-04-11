124cef7710ecbfab9d93330b009450e1cfe632e9
Tamara <tami.pg94@gmail.com>
2021-04-11 04:27:20 +0200

    first commit

---
+++ JS/app.js
@@ -0,0 +1,22 @@
+var nombre;
+var edad;
+var peso;
+var altura;
+var solucion;
+var resultadoInput;
+
+function calcIMC(){
+    nombre = document.getElementById("nombre").value;
+    edad = document.getElementById("edad").value;
+    peso = document.getElementById("peso").value;
+    altura = document.getElementById("altura").value;
+
+    solucion =  (peso / (altura * altura));
+    solucion = solucion.toFixed(2);
+
+    console.log("Su IMC es: " + solucion);
+    resultadoInput = document.getElementById("resultado");
+    resultadoInput.value = solucion;
+    return false;
+}
+
